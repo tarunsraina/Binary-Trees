91caf80eb3d256bc853eb9f0cfe79c3156005d31
tarun <tarunsraina483@gmail.com>
2021-07-03 21:43:57 +0530

    added some problems

--- Binary-tree/src/BinaryTreeProblems/EqualBinaryTrees.java
+++ Binary-tree/src/BinaryTreeProblems/EqualBinaryTrees.java
@@ -117,3 +117,35 @@ public class EqualBinaryTrees {
 
 
 }
+
+/*
+ *    This code cleared all test cases in leetcode:
+ * class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p==null && q==null)
+        {
+            return true;
+        }
+        
+        if(p==null || q==null)
+        {
+            return false;
+        }
+        
+        if(p.val != q.val)
+        {
+            return false;
+        }
+        
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
